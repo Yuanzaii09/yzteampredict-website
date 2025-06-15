115b67ede1a798fae1ca50743be472a9484a1c13
yu4nnn <yuanzaii09@gmail.com>
2025-06-15 16:56:06 +0800

    更新 script.js

--- basic/script.js
+++ basic/script.js
@@ -1,9 +1,7 @@
 document.getElementById("toggleSwitch").addEventListener("change", function () {
-        if (this.checked) {
-            console.log("开关开启");
-            // 可以在这里添加开启时的逻辑
-        } else {
-            console.log("开关关闭");
-            // 可以在这里添加关闭时的逻辑
-        }
-    });
+    if (this.checked) {
+        console.log("开关开启");
+    } else {
+        console.log("开关关闭");
+    }
+});
