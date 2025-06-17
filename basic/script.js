64cc07b8004f800cc3fa1d9524c5b55ef0faaaaf
yu4nnn <yuanzaii09@gmail.com>
2025-06-17 18:59:00 +0800

    更新 script.js

--- basic/script.js
+++ basic/script.js
@@ -3,4 +3,5 @@ const toggleButton = document.querySelector('.custom-toggle');
 
 toggleButton.addEventListener('click', () => {
     toggleContainer.classList.toggle('on');
+    toggleButton.classList.toggle('on');
 });
