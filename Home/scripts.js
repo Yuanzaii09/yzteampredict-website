e0aa13f3ec82c407cd80eb97a066adea0fec10a2
yu4nnn <yuanzaii09@gmail.com>
2025-07-07 21:09:49 +0800

    Delete Home/scripts.js

--- Home/scripts.js
+++
@@ -1,7 +0,0 @@
-const toggleContainer = document.querySelector('.toggle-container');
-const toggleButton = document.querySelector('.custom-toggle');
-
-toggleButton.addEventListener('click', () => {
-    toggleContainer.classList.toggle('on');
-    toggleButton.classList.toggle('on');
-});
